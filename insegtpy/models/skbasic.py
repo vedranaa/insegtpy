0f8f788d1c741f09c51c947552d1d90bba7a7921
Patrick M. Jensen <patmjen@dtu.dk>
2023-09-06 09:53:02 +0200

    Support multiple images when building dictionary

--- insegtpy/models/skbasic.py
+++ insegtpy/models/skbasic.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-""" Basic InSegt functionality. 
+""" Basic InSegt functionality.
 
-This module provides basic InSegt image processing functionallity. It uses 
+This module provides basic InSegt image processing functionallity. It uses
 intensities from image patches as features for clustering. For clustering it
-uses minibatch k-means from sklearn. 
+uses minibatch k-means from sklearn.
 
 
 Author: vand@dtu.dk, 2020
@@ -32,9 +32,9 @@ def image2patches(image, patch_size, stepsize=1):
             one image pixel. For stepsize 1, the number of returned patches
             is (X-patch_size+1)*(Y-patch_size+1) due to bounary. The length
             of columns is patch_size**2.
-    """    
+    """
     X, Y = image.shape
-    s0, s1 = image.strides    
+    s0, s1 = image.strides
     nrows = X-patch_size+1
     ncols = Y-patch_size+1
     shp = patch_size, patch_size, nrows, ncols
@@ -47,7 +47,7 @@ def ndimage2patches(im, patch_size, stepsize=1):
     """Rearrange image patches into columns for N-D image (e.g. RGB image)."""""
     if im.ndim == 2:
         return image2patches(im, patch_size, stepsize)
-    else:
+    elif im.ndim == 3:
         X ,Y, L = im.shape
         patches = np.zeros((L*patch_size*patch_size,
                             (X - patch_size + 1)*(Y - patch_size + 1)))
@@ -55,6 +55,19 @@ def ndimage2patches(im, patch_size, stepsize=1):
             patches[i*patch_size**2:(i+1)*patch_size**2,:] = image2patches(
                 im[:,:,i], patch_size, stepsize)
         return patches
+    elif im.ndim == 4:
+        N, X, Y, L = im.shape
+        patches_per_im = (X - patch_size + 1)*(Y - patch_size + 1)
+        patches = np.empty((L*patch_size*patch_size,
+                            N*patches_per_im))
+        for i in range(L):
+            for n in range(N):
+                patches[i*patch_size**2:(i+1)*patch_size**2,n*patches_per_im:(n+1)*patches_per_im] = image2patches(
+                    im[n,:,:,i], patch_size, stepsize
+                )
+        return patches
+    else:
+        raise ValueError('Image dimension not supported.')
 
 
 def image2assignment(image, patch_size, nr_clusters, nr_training_patches):
@@ -62,13 +75,19 @@ def image2assignment(image, patch_size, nr_clusters, nr_training_patches):
     patches = ndimage2patches(image, patch_size)
     patches_subset = patches[:,np.random.permutation(np.arange(patches.shape[1]))
                              [:nr_training_patches]]
-    kmeans = sklearn.cluster.MiniBatchKMeans(n_clusters = nr_clusters, 
+    kmeans = sklearn.cluster.MiniBatchKMeans(n_clusters = nr_clusters,
             max_iter = 10, batch_size = 3*nr_clusters)
     kmeans.fit(patches_subset.T)
     assignment = kmeans.predict(patches.T)
-    return (assignment.reshape((image.shape[0] - patch_size + 1, 
-                               image.shape[1] - patch_size + 1)),
-            kmeans)
+    if image.ndim < 4:
+        assignment = assignment.reshape((image.shape[0] - patch_size + 1,
+                                         image.shape[1] - patch_size + 1))
+    else:
+        assignment = assignment.reshape((image.shape[0],
+                                         image.shape[1] - patch_size + 1,
+                                         image.shape[2] - patch_size + 1))
+    return assignment, kmeans
+
 
 
 def new2assignment(image, kmeans):
@@ -76,30 +95,36 @@ def new2assignment(image, kmeans):
     patch_size = int(np.sqrt(kmeans.cluster_centers_.shape[1]))
     patches = ndimage2patches(image, patch_size)
     assignment = kmeans.predict(patches.T)
-    return assignment.reshape((image.shape[0] - patch_size + 1, 
+    return assignment.reshape((image.shape[0] - patch_size + 1,
                                image.shape[1] - patch_size + 1))
 
 
 def assignment2biadjacency(assignment, image_shape, patch_size, nr_clusters):
     """ Algorithm 1 from https://arxiv.org/pdf/1809.02226.pdf"""
-    n = image_shape[0]*image_shape[1]
+    # Ensure first dim is number of images
+    if len(image_shape) < 4:
+        image_shape = (1,) + image_shape
+    if assignment.ndim < 3:
+        assignment = assignment[None]
+    n = image_shape[0]*image_shape[1]*image_shape[2]
     m = patch_size*patch_size*nr_clusters
     s = (patch_size-1)//2
     # find displacement in i and j for within-patch positions dx and dy.
     dy, dx = np.meshgrid(np.arange(patch_size)-s,np.arange(patch_size)-s)
-    di = (dy + image_shape[1]*dx).ravel()
+    di = (dy + image_shape[2]*dx).ravel()
     dj = (dy + patch_size*dx).ravel()
     # populate index list for every assignment.
     i_accumulate = np.empty((assignment.size,patch_size**2))
     j_accumulate = np.empty((assignment.size,patch_size**2))
-    for x in range(assignment.shape[0]):
-        for y in range(assignment.shape[1]):
-            k = assignment[x,y]
-            i = (y+s) + (x+s)*image_shape[1] # linear index of the image pixel
-            j = s + s*patch_size + k*patch_size**2 # linear index of the patch center           
-            p = y + x*assignment.shape[1]
-            i_accumulate[p] = i+di
-            j_accumulate[p] = j+dj          
+    for b in range(assignment.shape[0]):
+        for x in range(assignment.shape[1]):
+            for y in range(assignment.shape[2]):
+                k = assignment[b,x,y]
+                i = (y+s) + (x+s)*image_shape[2] + b*image_shape[1]*image_shape[2] # linear index of the image pixel
+                j = s + s*patch_size + k*patch_size**2 # linear index of the patch center
+                p = y + x*assignment.shape[2] + b*assignment.shape[1]*assignment.shape[2]
+                i_accumulate[p] = i+di
+                j_accumulate[p] = j+dj
     B = scipy.sparse.coo_matrix((np.ones(i_accumulate.size, dtype=bool),
                     (i_accumulate.ravel(),j_accumulate.ravel())),shape=(n,m))
     return B.tocsr()
@@ -155,21 +180,24 @@ def gray_cool(nr_classes):
     colors = plt.cm.cool(np.linspace(0, 1, nr_classes))
     colors = np.r_[np.array([[0.5, 0.5, 0.5, 1]]), colors]
     cmap = matplotlib.colors.ListedColormap(colors)
-    return cmap 
+    return cmap
 
 
 def patch_clustering(image, patch_size, nr_training_patches, nr_clusters):
     """"InSegt preprocessing function: clustering, assignment and transformations."""
+    print('assign')
     assignment, kmeans = image2assignment(image, patch_size,
             nr_clusters, nr_training_patches)
+    print('B')
     B = assignment2biadjacency(assignment, image.shape, patch_size, nr_clusters)
+    print('T1 and T2')
     T1, T2 = biadjacency2transformations(B)
     return T1, T2, kmeans
 
 
 def two_binarized(labels, T1, T2):
     """InSegt processing function: from labels to probabilities."""
-    nr_classes = np.max(labels)    
+    nr_classes = np.max(labels)
     labcol = labels2labcol(labels, nr_classes=nr_classes) # columns with binary labels
     probcol = T2 * ((T1) * labcol) # first linear diffusion
     probcol = np.asarray(probcol.todense()) # columns with probabilities
@@ -178,105 +206,105 @@ def two_binarized(labels, T1, T2):
     probcol = T2*dict_labels # second linear diffusion
     probcol = np.asarray(probcol.todense())
     probabilities = np.transpose(probcol.reshape(labels.shape + (-1,)), (2,0,1))
-    return probabilities, dict_labels 
+    return probabilities, dict_labels
 
 
 def single_update(labels, T1, T2, nr_classes=None):
     """InSegt processing function: from labels to probabilities."""
-    if nr_classes is None: nr_classes = np.max(labels)    
+    if nr_classes is None: nr_classes = np.max(labels)
     labcol = labels2labcol(labels, nr_classes=nr_classes) # columns with binary labels
     dict_labels = T1 * labcol
     probcol = T2*dict_labels # second linear diffusion
     probcol = np.asarray(probcol.todense())
-    probabilities = np.transpose(probcol.reshape(labels.shape + (-1,)), (2,0,1))
-    return probabilities, dict_labels 
+    probabilities = np.moveaxis(probcol.reshape(labels.shape + (-1,)), -1, 0)
+    return probabilities, dict_labels
 
 
 def new2probs(image, kmeans, dict_labels):
     this_assignment = new2assignment(image, kmeans)
-        
+
     patch_size = int(np.sqrt(kmeans.cluster_centers_.shape[1]))
     nr_clusters = kmeans.cluster_centers_.shape[0]
 
     B = assignment2biadjacency(this_assignment, image.shape, patch_size, nr_clusters)
     T2 = biadjacency2transformations(B)[1]
-    probcol = T2 * dict_labels 
+    probcol = T2 * dict_labels
     probcol = np.asarray(probcol.todense())
     return np.transpose(probcol.reshape(image.shape[:2] + (-1,)), (2,0,1))
-    
-    
+
+
 # MAKING ALL OF THIS WORK WITH INSEGT
 class SkBasic(segt.Segt):
     ''' A class for basic processing, to comply with insegt.
     '''
 
     def __init__(self, image, patch_size, nr_training_patches, nr_clusters):
-        
-        T1, T2, kmeans = patch_clustering(image, patch_size, 
+
+        T1, T2, kmeans = patch_clustering(image, patch_size,
                         nr_training_patches, nr_clusters)
         self.T1 = T1
         self.T2 = T2
         self.kmeans = kmeans
 
         self.dict_labels = None
-    
+
     def process(self, labels, nr_classes=None):
         probabilities, dict_labels = single_update(labels, self.T1, self.T2, nr_classes)
         self.dict_labels = dict_labels
         return probabilities
-    
-    def segment_new(self, image):
-        
-        return new2probs(image, self.kmeans, self.dict_labels)
-       
 
-def sk_basic_segmentor(image, patch_size=3, nr_training_patches=1000, nr_clusters=100, 
+    def segment_new(self, image):
+
+        return new2probs(image, self.kmeans, self.dict_labels)
+
+
+def sk_basic_segmentor(image, patch_size=3, nr_training_patches=1000, nr_clusters=100,
              scales=None, propagation_repetitions=None):
-    ''' Convenience function to create a model based on SkBasic. '''    
-    
+    ''' Convenience function to create a model based on SkBasic. '''
+
     if scales is None or scales==1 or scales==[1]:
         print('Bulding single-scale SkBasic model.')
         model = SkBasic(image, patch_size, nr_training_patches, nr_clusters)
     else:
         print('Bulding multi-scale SkBasic model.')
         if type(scales) is not list: scales = [scales]
-        
-        model_init = lambda im: SkBasic(im, patch_size, 
+
+        model_init = lambda im: SkBasic(im, patch_size,
                                         nr_training_patches, nr_clusters)
         model = segt.Multiscale(image, scales, model_init)
-        
+
     if propagation_repetitions and propagation_repetitions>1:
         print('Adding propagation repetitions.')
         model = segt.Repeated(model, propagation_repetitions)
-        
+
     return model
-    
-        
+
+
 
 if __name__ == '__main__':
 
-# Example use without annotator (and without Segt)   
+# Example use without annotator (and without Segt)
 
     import PIL
     import utils
-    
+
     # Read image and labels.
     image = np.array(PIL.Image.open('../../data/glass.png'))
     labels = np.array(PIL.Image.open('../../data/glass_labels.png'))
     nr_classes = np.max(labels)
-        
+
     # Chooose settings.
     patch_size = 9
     nr_training_patches = 10000 # number of randomly extracted patches for clustering
     nr_clusters = 1000 # number of dictionary clusters
-    
+
     # Pre-process.
     T1, T2 = patch_clustering(image, patch_size, nr_training_patches, nr_clusters)[:2]
-    
+
     # Process.
     probabilities = two_binarized(labels, T1, T2)[0]
     segmentation  = utils.segment_probabilities(probabilities)
-    
+
     # Visualize.
     fig, ax = plt.subplots(1, 3, sharex = True, sharey = True, figsize=(15,5))
     ax[0].imshow(image, cmap='gray')
